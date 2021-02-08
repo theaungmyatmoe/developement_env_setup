9163d5094dd2076b4888db43be25fc7ebc9456b9
Aung Myat Moe <65492233+amm834@users.noreply.github.com>
2021-02-09 00:15:16 +0630

    Update dev_env_setup.sh

--- dev_env_setup.sh
+++ dev_env_setup.sh
@@ -1,1 +1,41 @@
-# will add soon
+#!/usr/bin/env bash
+
+if [[ $(uname -m) != "aarch64" ]]; then
+    echo -e "Your environment should be \e[32mAndroid (aarch64/arm64)\e[0m"
+    exit 1
+fi
+
+#remove motd file
+rm $PREFIX/etc/motd
+
+# install coreutils
+apt upgrade && apt update -y
+
+# install packages
+pkgs=("git" "wget" "zsh" "composer" "php" "apache" "php-apache" "mariadb" "rust")
+pkgsLength=${#pkgs[@]}
+for (( i = 0; i <= $pkgsLength; i++ )); do
+    pkg install ${pkgs[$i]} -y
+done
+
+# download server configuration file via wget
+wget https://raw.githubusercontent.com/amm834/developement_env_setup/main/httpd.conf
+
+
+# remove default configuration file
+rm $PREFIX/etc/apache2/httpd.conf
+
+# move downloaded configuration file
+mv httpd.conf $PREFIX/etc/apache2
+
+# download service file
+wget https://raw.githubusercontent.com/amm834/developement_env_setup/main/service
+
+# change as executable file
+chmod +x service
+
+# move service file to bin
+mv service $PREFIX/bin/
+
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
